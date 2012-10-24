3305952e0ce3d4552f8d984027838ed8b31d6d21
nesmanuel <dmnt_nes@outlook.com>
2012-10-24 10:00:23 -0500

    comentarios
    modulo que permite añadir comentarios

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geosocial::Application.config.secret_token = 'a232a344962d5e1064e5844d1d2ee5ca5d28fa854e33799aea4f68eb6bb22b0fcbba72b45d293e1f9bd4f7768deb02739a7b133f72e1aa0c0f49014995b33ddb'
